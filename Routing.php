878558eb80d86759e7f0735871b08ecc0733c196
Kupsztajn <nawrocki.jakub@gmail.com>
2025-10-22 16:05:56 +0200

    lab3

--- Routing.php
+++ Routing.php
@@ -1,14 +1,34 @@
 <?php
-
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
 class Routing {
+
+    public static $routes = [
+        'login' => 'SecurityController@login',
+        'logout' => 'SecurityController@logout',
+        'dashboard' => 'DashboardController@dashboard'
+    ];
+
     public static function run($path) {
         switch ($path) {
             case 'dashboard':
-                include 'public/views/dashboard.html';
+                
                 break;
+
             case 'login':
-                include 'public/views/login.html';
+                // Pobierz wpis z tablicy routes
+                $route = self::$routes[$path]; // "SecurityController@login"
+
+                // Rozdziel na nazwę kontrolera i metodę
+                list($controllerName, $action) = explode('@', $route);
+
+                // Utwórz obiekt kontrolera
+                $controller = new $controllerName();
+
+                // Wywołaj metodę kontrolera
+                $controller->$action();
                 break;
+
             default:
                 include 'public/views/404.html';
                 break;
---
+++ src/controllers/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'AppController.php';
+
+class SecurityController extends AppController {
+
+    public function login()
+    {
+        // TODO pobieramy z formularza email, haslo
+        // todo sprawdzamy czy taki user istnieje w db
+        // jezeli nie istnieje to zwracamy odpowiednie komunikaty
+        //jezeli istnieje to logujemy usera (tworzymy sesje)
+        return $this->render('login');
+    }
+
+    public function logout()
+    {
+        // Logic for logging out the user
+        session_start();
+        session_destroy();
+        header('Location: /login');
+        exit();
+    }
+
+}
